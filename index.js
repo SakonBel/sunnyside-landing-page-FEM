433daf858df8f63622ec649ef41c66fb9230d9b9
Sakon Plangklang <iamsabell@gmail.com>
2021-06-30 23:49:21 +0700

    Add more interaction

--- index.js
+++ index.js
@@ -4,15 +4,38 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-list");
 const navItem = document.querySelector("nav ul");
 
-hamburger.addEventListener("click", toggleMenu);
-body.addEventListener("scroll", vanish);
+let scrollEndTop = 0;
+let status = { active: false };
 
-function toggleMenu() {
+hamburger.addEventListener("click", () => toggleMenu(status.active));
+
+window.addEventListener("scroll", scrollEffect);
+
+function toggleMenu(active) {
+  // body.classList.toggle("ham-active");
   navMenu.classList.toggle("nav-active");
   hamburger.classList.toggle("ham-active");
   navItem.classList.toggle("nav-item-active");
+  status.active = !active;
 }
 
-function vanish() {
-  nav.classList.toggle("appear");
+function scrollEffect() {
+  let scrollTop = window.pageYOffset;
+  let vanish;
+
+  if (scrollTop > 0 && scrollTop < 100) {
+    nav.style.top = `-${scrollTop}px`;
+  } else if (scrollTop > scrollEndTop) {
+    if (status.active) {
+      nav.style.top = "0";
+    } else {
+      nav.style.top = `-100px`;
+    }
+  } else {
+    nav.style.top = "0";
+    // vanish = setTimeout(() => {
+    //   nav.style.top = "-100px";
+    // }, 2000);
+  }
+  scrollEndTop = scrollTop;
 }
